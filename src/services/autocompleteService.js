a70df27fe281e1a8ffcac98b93aa98aecd42511c
Einav Ben Shimshon <einavshazo@gmail.com>
2024-10-14 13:54:09 +0300

    Update the API call - Updating the server's URL #3

--- src/services/autocompleteService.js
+++ src/services/autocompleteService.js
@@ -1,5 +1,5 @@
 export async function fetchAutocompleteResults(query) {
-    const response = await fetch(`deloitte-employee.vercel.app/api/autocomplete?q=${query}`);
+    const response = await fetch(`https://deloitte-employee.vercel.app/api/autocomplete?q=${query}`);
     if (!response.ok) {
       throw new Error('Error fetching autocomplete results');
     }
